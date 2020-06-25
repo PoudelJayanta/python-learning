aea72c832745349968fffd4bbd575eacb9c4b1b2
Poudel Jayanta <49644302+PoudelJayanta@users.noreply.github.com>
2020-06-25 18:46:26 +0545

    Add files via upload

--- list_int_filtering.py
+++ list_int_filtering.py
@@ -3,8 +3,8 @@ def func(x):
   for i in x:
     if (type(i)==int):
       y.append(i)
-    else:
-      pass
+  return y
   print(y)
 l=[1,5,"ram",10, "apple",12, 70, "mango"]
-func(l)
+z=func(l)
+print(z)
